f0624071e2f37df30463be6b0f17e6e0ece308f5
irinaugrak <irina.ugrak@gmail.com>
2019-02-22 14:39:56 +0300

    установка шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('sass', function(){ // Создаем таск Sass
-  return gulp.src('app/sass/**/*.sass') // Берем источник
+  return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss']) // Берем источник
     .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
     .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
     .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
@@ -22,8 +22,8 @@ gulp.task('html', function(){ // Создаем таск Sass
   return gulp.src('app/**/*.html').pipe(browserSync.reload({stream: true}))// Обновляем CSS на странице при изменении
 });
 
-gulp.task('watch', gulp.series('browser-sync', 'sass'), function() {
-  gulp.watch('app/sass/**/*.sass', gulp.series('sass')); // Наблюдение за sass файлами
+gulp.task('watch', function() {
+  gulp.watch(['app/sass/**/*.sass', 'app/sass/**/*.scss'], gulp.series('sass')); // Наблюдение за sass файлами
   gulp.watch('app/*.html', gulp.series('html'));
   // Наблюдение за другими типами файлов
 });
