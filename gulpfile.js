beee11e9b37d1c92e7424cc1c7acbc29ebb5208b
irinaugrak <irina.ugrak@gmail.com>
2019-02-22 11:08:41 +0300

    настройка окружения, создание структуры проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
-var gulp = require('gulp'), // Подключаем Gulp
-    sass = require('gulp-sass'); // Подключаем Sass пакет
+var gulp      = require('gulp'), // Подключаем Gulp
+  sass        = require('gulp-sass'), //Подключаем Sass пакет,
+  browserSync = require('browser-sync'); // Подключаем Browser Sync
 
-gulp.task('sass', function() { // Создаем таск "sass"
-    return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+  browserSync({ // Выполняем browserSync
+    server: { // Определяем параметры сервера
+      baseDir: 'app' // Директория для сервера - app
+    },
+    notify: false // Отключаем уведомления
+  });
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], gulp.series('sass')); // Наблюдение за sass файлами в папке sass
+gulp.task('sass', function(){ // Создаем таск Sass
+  return gulp.src('app/sass/**/*.sass') // Берем источник
+    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+    .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('html', function(){ // Создаем таск Sass
+  return gulp.src('app/**/*.html').pipe(browserSync.reload({stream: true}))// Обновляем CSS на странице при изменении
+});
+
+gulp.task('watch', gulp.series('browser-sync', 'sass'), function() {
+  gulp.watch('app/sass/**/*.sass', gulp.series('sass')); // Наблюдение за sass файлами
+  gulp.watch('app/*.html', gulp.series('html'));
+  // Наблюдение за другими типами файлов
 });
 
 gulp.task('default', gulp.series('watch'));
-
-
-
-
-
